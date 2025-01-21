3f36156f4e593c6fa41d4b3fe5be80458e4ddb95
Bhavya Nanda <bhavya.n@browserstack.com>
2025-01-21 13:31:33 +0530

    inegrated with aut and o11y

--- features/pages/linkFuncs.js
+++ features/pages/linkFuncs.js
@@ -1,36 +1,4 @@
 
-// class linkFunc{
-
-//   async goToURL(theURL){
-//     await browser.url(`${theURL}`);
-
-//   }
-//     /**
-//    * Wait for an element to be visible
-//    * @param {string} xPaath - The XPath of the element
-//    */
-//   async checkVisible(xPaath){
-//     await browser.useXpath().waitForElementVisible(xPaath, 10000);
-//   }
-//   async clickLink(xPaath){
-//     await browser.useXpath().click(xPaath);
-//     await browser.pause(3000); 
-//   }
-//  async getPresentURL(){
-//   console.log(`Clicking link with href: ${hrefValue}`);
-//   const currentURL = await browser.getCurrentUrl();
-//  }
-//  async ValidateURL(){
-//   console.log(`Current URL after click: ${currentURL}`);
-//   browser.expect(currentURL).to.contain(expectedURL);
-//   await browser.back();
-//   await browser.pause(3000); 
-//  }
-
-// }
-
-// module.exports = new linkFunc();
-
 class linkFunc {
   /**
    * Navigate to a specific URL
@@ -67,6 +35,15 @@ class linkFunc {
     return currentURL;
   }
 
+/** 
+ * check title of the page
+  @param {string} title
+  */
+  async checkTitle(title){
+    await browser.expect.title().to.equal(title);  
+  }
+
+
   /**
    * Validate if the current URL matches the expected pattern
    * @param {string} expectedURL - The expected URL pattern
@@ -84,7 +61,6 @@ class linkFunc {
     const logs = await browser.getLog('browser');  // Capture browser logs
     logs.forEach(log => {
       const message = log.message;
-      // If the message is an object, try to log specific properties
         console.log(`Log Type: ${log.level}, Timestamp: ${log.timestamp}`);
         console.log('Log Message:', JSON.stringify(message, null, 2));  // Pretty-print the object 
 
--- features/step_definitions/linksVal.js
+++ features/step_definitions/linksVal.js
@@ -1,6 +1,5 @@
 const { Given, When, Then , After } = require('@cucumber/cucumber');
 const linkFunc= require('../pages/linkFuncs.js');
-const locatorsPage = require('/Users/bhavyananda/Desktop/Initial scripts/ass-1-v2/features/locators/locators.js');
 
 Given('I open the Automation Practice page for links', async () => {
   const theURL= 'https://ultimateqa.com/automation/';
@@ -23,48 +22,9 @@ Then('I should be redirected to the page with URL containing {string}', async (e
 
   const currentURL2 = await linkFunc.getPresentURL();
   console.log(`headed back to ${currentURL2}`);
-  // dummy error
 });
 
 After(async () => {
   await linkFunc.captureConsoleEvents();
 });
 
-/*
- const { Given, When, Then } = require('@cucumber/cucumber');
-const linkFunc = require('../pages/linkFuncs.js'); // Correct relative path
-
-Given('I open the Automation Practice page for links', async () => {
-  const theURL = 'https://ultimateqa.com/automation/';
-  await linkFunc.goToURL(theURL);
-});
-
-When('I find and click the link with href {string}', async (hrefValue) => {
-  const xPaath = `//a[@href='${hrefValue}']`;
-  console.log(`Generated XPath: ${xPaath}`);
-
-  await linkFunc.checkVisible(xPaath);
-  await linkFunc.clickLink(xPaath);
-});
-
-Then('I should be redirected to the page with URL containing {string}', async (expectedURL) => {
-  const currentURL = await linkFunc.getPresentURL();
-  await linkFunc.ValidateURL(expectedURL, currentURL);
-});
-*/
-
-
-//js errros logging
-// DOUBT!!
-
-// Then('I fetch the browser console logs', async()=>{
-//   await browser.captureConsoleLogs((log) => {
-//       // Safely check for args existence and handle it
-//       if (log.args && Array.isArray(log.args)) {
-//         console.log(`[Console ${log.level}] ${log.timestamp} - ${log.args.map(arg => arg.value).join(' ')}`);
-//       } else {
-//         console.log(`[Console ${log.level}] ${log.timestamp} - No arguments available.`);
-//       }
-//   });
-  
-// })
